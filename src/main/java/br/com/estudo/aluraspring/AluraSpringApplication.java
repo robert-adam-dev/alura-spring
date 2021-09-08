a28df64efb8aa4bd038eec054fa7c82e59ceddd5
Robert Adam <r.neves1992@gmail.com>
2021-09-08 17:01:25 -0300

    adicionando paginação

--- src/main/java/br/com/estudo/aluraspring/AluraSpringApplication.java
+++ src/main/java/br/com/estudo/aluraspring/AluraSpringApplication.java
@@ -2,8 +2,10 @@ package br.com.estudo.aluraspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class AluraSpringApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/estudo/aluraspring/repository/TopicoRepository.java
+++ src/main/java/br/com/estudo/aluraspring/repository/TopicoRepository.java
@@ -1,11 +1,13 @@
 package br.com.estudo.aluraspring.repository;
 
 import br.com.estudo.aluraspring.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-    List<Topico> findByCursoNome(String nomeCurso);
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 }
